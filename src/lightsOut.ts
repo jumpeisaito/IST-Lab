ccf9f800da8c6b73e94b02cc6f85653d1f05f969
specinef <kspecinef@gmail.com>
2014-09-30 00:35:59 +0900

    complete develop

--- src/lightsOut.ts
+++ src/lightsOut.ts
@@ -23,29 +23,85 @@ function LightsOut() {
     });
   }
   self.clickCell = function(data, event) {
-    console.log(data);
-    switch (event.currentTarget.attributes.col.value) {
-      case "0":
-        self.lightRows()[event.currentTarget.attributes.row.value].c0(
-          !self.lightRows()[event.currentTarget.attributes.row.value].c0());
+    var row = _.parseInt(event.currentTarget.attributes.row.value);
+    var col = _.parseInt(event.currentTarget.attributes.col.value)
+    switch (col) {
+      case 0:
+        self.lightRows()[row].c0(!self.lightRows()[row].c0());
+        self.lightRows()[row].c1(!self.lightRows()[row].c1());
+        if (row !== 0) {
+          self.lightRows()[row - 1].c0(!self.lightRows()[row - 1].c0());
+        }
+        if (row !== 4) {
+          self.lightRows()[row + 1].c0(!self.lightRows()[row + 1].c0());
+        }
         break;
-      case "1":
-        self.lightRows()[event.currentTarget.attributes.row.value].c1(
-          !self.lightRows()[event.currentTarget.attributes.row.value].c1());
+      case 1:
+        self.lightRows()[row].c0(!self.lightRows()[row].c0());
+        self.lightRows()[row].c1(!self.lightRows()[row].c1());
+        self.lightRows()[row].c2(!self.lightRows()[row].c2());
+        if (row !== 0) {
+          self.lightRows()[row - 1].c1(!self.lightRows()[row - 1].c1());
+        }
+        if (row !== 4) {
+          self.lightRows()[row + 1].c1(!self.lightRows()[row + 1].c1());
+        }
         break;
-      case "2":
-        self.lightRows()[event.currentTarget.attributes.row.value].c2(
-          !self.lightRows()[event.currentTarget.attributes.row.value].c2());
+      case 2:
+        self.lightRows()[row].c1(!self.lightRows()[row].c1());
+        self.lightRows()[row].c2(!self.lightRows()[row].c2());
+        self.lightRows()[row].c3(!self.lightRows()[row].c3());
+        if (row !== 0) {
+          self.lightRows()[row - 1].c2(!self.lightRows()[row - 1].c2());
+        }
+        if (row !== 4) {
+          self.lightRows()[row + 1].c2(!self.lightRows()[row + 1].c2());
+        }
         break;
-      case "3":
-        self.lightRows()[event.currentTarget.attributes.row.value].c3(
-          !self.lightRows()[event.currentTarget.attributes.row.value].c3());
+      case 3:
+        self.lightRows()[row].c2(!self.lightRows()[row].c2());
+        self.lightRows()[row].c3(!self.lightRows()[row].c3());
+        self.lightRows()[row].c4(!self.lightRows()[row].c4());
+        if (row !== 0) {
+          self.lightRows()[row - 1].c3(!self.lightRows()[row - 1].c3());
+        }
+        if (row !== 4) {
+          self.lightRows()[row + 1].c3(!self.lightRows()[row + 1].c3());
+        }
         break;
-      case "4":
-        self.lightRows()[event.currentTarget.attributes.row.value].c4(
-          !self.lightRows()[event.currentTarget.attributes.row.value].c4());
+      case 4:
+        self.lightRows()[row].c3(!self.lightRows()[row].c3());
+        self.lightRows()[row].c4(!self.lightRows()[row].c4());
+        if (row !== 0) {
+          self.lightRows()[row - 1].c4(!self.lightRows()[row - 1].c4());
+        }
+        if (row !== 4) {
+          self.lightRows()[row + 1].c4(!self.lightRows()[row + 1].c4());
+        }
         break;
       default:
+    }
+    self.lightsCheck();
+  }
+  
+  self.lightsCheck = function() {
+    var count = 0;
+    // 本当はanyキャストじゃなく、ちゃんと型を付けたいですね
+    _.forEach(self.lightRows(), (row: any) => {
+      if (
+        row.c0() &&
+        row.c1() &&
+        row.c2() &&
+        row.c3() &&
+        row.c4()
+      ) {
+        count++;
+      } else {
+        return false;
+      }
+    });
+    if (count === 5) {
+      window.alert("clear!!");
     }
   }
 }
